ae9d3f46a3519d65d66c120acf587a1c3fa31c71
yennanliu <f339339@gmail.com>
2020-09-01 23:30:55 +0800

    fix test TestCommonConfig.scala

--- src/test/scala/TestCommonConfig.scala
+++ src/test/scala/TestCommonConfig.scala
@@ -8,10 +8,12 @@ import common.CommonConfig
 class TestCommonConfig extends AnyFunSuite {
 
   val kafka_user = CommonConfig.kafka_user
+  val kafka_topic = CommonConfig.kafka_topic
+  val kafka_consumer = CommonConfig.kafka_consumer
 
-  test("kafka default user should equal kafka_user") {
-    //assert(kafka_user == "kafka_user")
-    assert( 123 == 123)
+  test("kafka default config should as the one in application.conf") {
+    assert(kafka_user == "kafka_user")
+    assert(kafka_topic == "kafka_topic")
+    assert(kafka_consumer == "kafka_consumer")
   }
-
 }
