f5d0391c2f41228a97d7c55b7b130ba7abd17dbb
yennanliu <f339339@gmail.com>
2020-09-19 14:05:50 +0800

    add logging (TestDatetimeUtils.scala), fix format(common/CommonConfig.scala

--- src/main/scala/common/CommonConfig.scala
+++ src/main/scala/common/CommonConfig.scala
@@ -4,19 +4,14 @@ package common
 import com.typesafe.config.{Config, ConfigFactory}
 
 object CommonConfig{
-
     private val cfg = ConfigFactory.load()
-
     val env: String = if (cfg.hasPath("env")) cfg.getString("env") else "default"
-
     val config: Config = cfg
         .getConfig(env)
         .withFallback(cfg.getConfig("default"))
 
     val kafka_user: String = config.getString("user")
-
     val kafka_topic: String = config.getString("topic")
-
     val kafka_consumer: String = config.getString("consumer")
 
 }
--- src/main/scala/dev/TestDatetimeUtils.scala
+++ src/main/scala/dev/TestDatetimeUtils.scala
@@ -1,18 +1,35 @@
 package dev
 
+import org.apache.log4j.Logger
 //UDF
 import common.DatetimeUtils._
 
 object TestDatetimeUtils extends App{
-  println("run TestDatetimeUtils ...")
-  println(s"now : $now")
-  println(s"nowLong : $nowLong")
 
-  val time1 = DateFormat.parseDateTime("2005-07-30")
-  val time2 = yearMonthFormat.parseDateTime("2005-07")
-  val time3 = DateHourFormat.parseDateTime("2005-07-30 11")
+  private val LOG = Logger.getLogger(this.getClass.getName)
+  RunBasicDateTimeUtils(LOG)
+  RunBasicDateTimeUtils2(LOG)
 
-  println(s"time1 : $time1")
-  println(s"time2 : $time2")
-  println(s"time3 : $time3")
+  def RunBasicDateTimeUtils(log: Logger): Unit = {
+    log.info("run TestDatetimeUtils ...")
+    println("run TestDatetimeUtils ...")
+
+    println(s"now : $now")
+    println(s"nowLong : $nowLong")
+
+    val time1 = DateFormat.parseDateTime("2005-07-30")
+    val time2 = yearMonthFormat.parseDateTime("2005-07")
+    val time3 = DateHourFormat.parseDateTime("2005-07-30 11")
+
+    println(s"time1 : $time1")
+    println(s"time2 : $time2")
+    println(s"time3 : $time3")
+  }
+
+  def RunBasicDateTimeUtils2(log: Logger): Unit = {
+    log.info("run RunBasicDateTimeUtils2 ...")
+    println("run RunBasicDateTimeUtils2 ...")
+    println(s"now : $now")
+    println(s"nowLong : $nowLong")
+  }
 }
