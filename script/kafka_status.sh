7c828632d91896f65ac91f8b97c904f2094ce211
yennanliu <f339339@gmail.com>
2020-09-26 16:22:10 +0800

    update kafka_status.sh

--- script/kafka_status.sh
+++ script/kafka_status.sh
@@ -5,25 +5,37 @@
 ZK_IP=127.0.0.1:2181
 BOOTSTRAP_SERVER_IP=127.0.0.1:9092
 
+show_topics(){
 echo ">>> SHOW ALL KAFKA TOPICS" 
 kafka-topics  --zookeeper $ZK_IP --list
+}
 
+describe_topics(){
 echo ">>> DESCRIBE KAFKA TOPICS"
 topics=$(kafka-topics  --zookeeper $ZK_IP --list)
 for topic in ${topics[@]} 
 do
     echo "topic = " $topic 
     kafka-topics  --zookeeper  $ZK_IP --topic $topic --describe 
-done 
+done
+}
 
+list_groups(){
 echo ">>> LIST CONSUMER GROUPS"
 kafka-consumer-groups --bootstrap-server  $BOOTSTRAP_SERVER_IP --list
+}
 
+describe_consumers(){
 echo ">>> DESCRIBE THE CONSUMER"
 consumer_groups=$(kafka-consumer-groups  --bootstrap-server  $BOOTSTRAP_SERVER_IP --list)
 for consumer_group in ${consumer_groups[@]} 
 do
     echo "consumer_group = " $consumer_group 
     kafka-consumer-groups --bootstrap-server $BOOTSTRAP_SERVER_IP --describe --group $consumer_group
-done 
+done
+}
 
+show_topics
+#describe_topics
+list_groups
+#describe_consumers
